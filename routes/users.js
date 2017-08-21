839dd9ffb774eae104088fe51cd4e82d58e1ac4d
Flavie Laurens <flavie.laurens@felixideas.ch>
2017-08-21 15:15:01 +0200

    routes and json and api

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,28 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
+router.get('/', function(req, res) {
+  models.user.findAll().then(function(users) {
+    res.render("users", {"title": "Users", "users": users});
+  });
+});
+
+router.get('/json', function(req, res) {
+  models.user.findAll().then(function(users) {
+    res.json(users);
+  });
+});
+
+router.get('/:id', function(req, res) {
+  models.user.findAll({
+    where: {
+      "id": req.params.id
+    }
+  }).then(function(user) {
+    res.json(user)
+  });
+});
+
 router.post('/', function(req, res) {
   var user = req.body.user;
   models.user.create(user).then(function() {
@@ -9,23 +31,18 @@ router.post('/', function(req, res) {
   });
 });
 
-router.get('/', function(req, res) {
-  models.user.findAll().then(function(users) {
-    res.render("users", {"title": "Users", "users": users});
-  });
-});
 
-router.get('/:id', function(req, res) {
+router.put('/:id', function(req, res) {
   models.user.update({
     where: {
       "id": req.params.id
     }
   }).then(function() {
-    res.redirect('users')
+    res.redirect('/');
   });
 });
 
-router.get('/:id/delete', function(req, res) {
+router.delete('/:id', function(req, res) {
   models.user.delete({
     where: {
       "id": req.params.id
